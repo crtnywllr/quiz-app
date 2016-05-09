74d74c4025c025db80f0cdfe32eb4899e3380e31
Courtney Waller <wllr.crtny.jyc@gmail.com>
2016-05-09 23:51:39 +0000

    cleaning up comments

--- public/js/app.js
+++ public/js/app.js
@@ -1,77 +1,4 @@
 //Step 1 Declare Variables
-//Set Questions
-/*var questions = [{
-    //Question 1
-    question: "If you were strolling along this street, where would you be?",
-    imageUrl: 'images/Montmartre.jpg',
-    choices: ["Paris, France", 'Prague, Czech Republic', 'Montreal, Canada'],
-    correct: 0,
-    details: "Paris, France: This is the famous Montmartre neighborhood."
-}, {
-    //Question 2
-    question: "Which beautiful country has 10 times more sheep than people?",
-    imageUrl: 'images/nz-sheep.jpg',
-    choices: ["Ireland", 'South Africa', 'New Zealand'],
-    correct: 2,
-    details: "New Zealand: Also home to the flightless and endangered kiwi bird."
-}, {
-    //Question 3
-    question: "This popular restaurant street can be found in...",
-    imageUrl: 'images/guijie.jpg',
-    choices: ["Manila, Phillipines", 'Beijing, China', 'Kuala Lumpur, Malaysia'],
-    correct: 1,
-    details: "Beijing, China: Guijie (aka Ghost Street) is a popular spot for customers and vendors. "
-}, {
-    //Question 4
-    question: "This country is famous for this kind of arena.",
-    imageUrl: 'images/Ronda.jpg',
-    choices: ["United States", 'Spain', 'Brazil'],
-    correct: 1,
-    details: "Spain: This bullfighting arena is in Ronda."
-}, {
-    //Question 5
-    question: "Where can you find this majestic scene?",
-    imageUrl: 'images/us-bison.jpg',
-    choices: ["United States", 'Poland', 'Australia'],
-    correct: 0,
-    details: "United States: The North American bison is related to, but distinct from, species of buffalo found elsewhere in the world."
-}, {
-    //Question 6
-    question: "Where is this iconic building located?",
-    imageUrl: 'images/red-square.jpg',
-    choices: ["Istanbul, Turkey", 'Berlin, Germany', 'Moscow, Russia'],
-    correct: 2,
-    details: "Moscow, Russia: This is the famous Red Square."
-}, {
-    //Question 7
-    question: "Which country offers this view?",
-    imageUrl: 'images/patagonia.jpg',
-    choices: ["Chile", 'New Zealand', 'Canada'],
-    correct: 0,
-    details: "Chile: Patagonia is an adventure lover's paradise."
-}, {
-    //Question 8
-    question: "This very old and very famous site is found in...",
-    imageUrl: 'images/angkor-wat.jpg',
-    choices: ["India", 'Cambodia', 'China'],
-    correct: 1,
-    details: "Cambodia - Angkor Wat was built in the early 12th century."
-}, {
-    //Question 9
-    question: "Bright colors and narrow alleys are common sights in this country.",
-    imageUrl: 'images/Morocco.jpg',
-    choices: ["Thailand", 'Croatia', 'Morocco'],
-    correct: 2,
-    details: "Morocco: Chefchaouen in particular is known for its blue buildings."
-}, {
-    //Question 10
-    question: "If you were mooring your sailboat in this harbor, what nation would you be in?",
-    imageUrl: 'images/victoria-harbor.jpg',
-    choices: ["England", 'Canada', 'Australia'],
-    correct: 1,
-    details: "Canada: Victoria Harbor is a popular entrance to Vancouver Island for those coming by boat or seaplane."
-}];*/
-//Other Variables
 var questionNum = 0;
 var numCorrect = 0;
 var questionList;
@@ -79,7 +6,7 @@ var userAnswers = [];
 //var questionTotal = questions.length;
 
 //Step 2 Declare Functions
-//Show questions
+//Get questions from server
 function getQuestions() {
    $.ajax('/questions', {
             method: 'GET',
@@ -92,6 +19,7 @@ function getQuestions() {
         });
 };
 
+//Send user answer and compare with backend; store answer response
 function getAnswer(userGuess){
     var stuff = {
         'question': questionNum,
@@ -114,13 +42,9 @@ function getAnswer(userGuess){
         currentQuestion.details = details; 
         nextQuestion(questionList);
     })
-    // get the response
-    // response will contain yaya , or nay
-    // response will contain answer for current question
-    
-}
+ }
 
-
+//Display questions
 
    function displayQuestions(questions) {
     $('#userChoices').empty();
@@ -136,14 +60,13 @@ function getAnswer(userGuess){
     $('#question-counter').text("Question " + (questionNum + 1) + "/" + questions.length);
     //update score tracker
     $('#score').text('Score ' + (numCorrect) + "/" + questions.length);
-
   
 }
 
-//loading questions
+//Loading next question
 function nextQuestion(questionList) {
    
-    //submit final question
+    //Appending info to final answers
       var htmlOutput = "";
       htmlOutput += '<div class="answers">';
       htmlOutput += '<div class="answer-text"> <h3>' + (questionNum + 1) + ') ' + 'Q: ' + questionList[questionNum].question + '</h3>';
@@ -152,6 +75,8 @@ function nextQuestion(questionList) {
       htmlOutput += '<div class="answer-image"><img src ="' + questionList[questionNum].imageUrl + '" class="image-class" /></div>';
       htmlOutput += '</div>';
       $('.answers-container').append(htmlOutput);
+      
+    //Submitting final question  
     if (questionNum + 1 == questionList.length) {
         //update final score at top
         $('#finalScore').text("You got " + numCorrect + '/' + questionList.length + ' right!')
@@ -159,7 +84,7 @@ function nextQuestion(questionList) {
         $('.questions').hide();
         $('.introduction').hide();
         $('.final').show();
-    } //load another question
+    } //Load another question
     else {
         questionNum++;
         displayQuestions(questionList); 
@@ -167,7 +92,7 @@ function nextQuestion(questionList) {
 }
 
 
-//try again
+//Reset quiz
 function tryAgain() {
     document.location.reload(true);
     //alert('I have activated the tryAgain function')
@@ -175,12 +100,12 @@ function tryAgain() {
 
 //Step 3 Use Functions
 $(document).ready(function () {
-    //on page load
+    //On page load
     $('.questions').hide();
     $('.final').hide();
     $('.introduction').show();
 
-    //starts quiz
+    //Starts quiz
     $('#start').on('click', function () {
         $('.introduction').hide();
         $('.final').hide();
@@ -188,7 +113,7 @@ $(document).ready(function () {
         getQuestions();
     })
 
-    //hits guess button
+    //Hits guess button
     $('#submit').on('click', function (e) {
         e.preventDefault();
         if ($("#userChoices input[id='option']").is(':checked')) {
@@ -201,7 +126,7 @@ $(document).ready(function () {
         }
     })
 
-    //hits try again button
+    //Hits try again button
     $('#reset').on('click', function () {
         tryAgain();
     })
